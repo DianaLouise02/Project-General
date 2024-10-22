ffc7a7089beaf0ad81e74d893404a3bd75cd443e
Diana Louise <111836528+DianaLouise02@users.noreply.github.com>
2024-10-22 13:53:03 -0300

    Add files via upload

---
+++ MainNavigator.js
@@ -0,0 +1,24 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import Home from './src/Pages/Home';
+import Sobre from './src/Pages/Sobre';
+import Contato from './src/Pages/Contato';
+import RedesSociais from './src/Pages/RedesSociais';
+import Faq from './src/Pages/Faq';
+const Stack = createNativeStackNavigator();
+
+
+export default function MainNavigator() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName = "Home">
+                <Stack.Screen name = "Home" component = {Home}  options={{headerShown: false }} />
+                <Stack.Screen name = "Sobre" component = {Sobre} />
+                <Stack.Screen name = "Contato" component = {Contato} />
+                <Stack.Screen name = "RedesSociais" component = {RedesSociais} />
+                <Stack.Screen name = "Faq" component = {Faq} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
